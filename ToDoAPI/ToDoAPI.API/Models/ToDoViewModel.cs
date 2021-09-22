4bf814a81e61dfb55c8cdb11191b8a6d876c7d7f
mikeprussell <mikeprussell@outlook.com>
2021-09-22 12:49:48 -0500

    Edmx and ToDoViewModel added

---
+++ ToDoAPI/ToDoAPI.API/Models/ToDoViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;//added for validation
+
+namespace ToDoAPI.API.Models
+{
+    public class ToDoViewModel
+    {
+        [Key]
+        public int TodoId { get; set; }
+
+        [Required]
+        public string Action { get; set; }
+
+        [Required]
+        public bool Done { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public virtual CategoryViewModel Category { get; set; }
+
+    }
+
+    public class CategoryViewModel
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "** Max 50 characters")]
+        public string CategoryName { get; set; }
+
+        [StringLength(100, ErrorMessage = "** Max 100 characters")]
+        public string CategoryDescription { get; set; }
+
+    }
+}
+}
